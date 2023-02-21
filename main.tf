fe42d26158eb088a1e2b9b9bd99659ee50a61808
prakriti07 <57622944+prakriti07@users.noreply.github.com>
2023-02-21 15:12:25 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,15 +9,29 @@ module "management_network" {
   region      = var.region
 }
 
-# Create the bastion host to access private instances
-module "bastion_host" {
-  source = "../modules/bastion-host"
-  instance_name = "bastion-vm"
-  subnetwork    = module.management_network.public_subnetwork
-  project = var.project
-  zone    = var.zone
+# Create an instance with OS Login configured to use as a bastion host
+resource "google_compute_instance" "bastion_host" {
+  project      = var.project
+  name         = "bastion-vm"
+  machine_type = "t2a-standard-1"
+  zone         = var.zone
+  tags = [var.tag]
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts-arm64"
+    }
+  }
+  network_interface {
+    subnetwork = module.management_network.public_subnetwork
+    // If var.static_ip is set use that IP, otherwise this will generate an ephemeral IP
+    access_config {
+      nat_ip = var.static_ip
+    }
+  }
+  metadata = {
+    enable-oslogin = "TRUE"
+  }
 }
-
 # Create a private instance to use alongside the bastion host.
 resource "google_compute_instance" "private" {
   project = var.project

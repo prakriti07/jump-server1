5734e2100cd3dc9486e28ee281729cc9e8a78523
prakriti07 <57622944+prakriti07@users.noreply.github.com>
2023-02-22 11:48:25 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -12,3 +12,20 @@ variable "zone" {
   description = "The zone in which the bastion host VM instance will be launched. Must be within the region."
   type        = string
 }
+
+# ---------------------------------------------------------------------------------------------------------------------
+# OPTIONAL PARAMETERS
+# Generally, these values won't need to be changed.
+# ---------------------------------------------------------------------------------------------------------------------
+
+variable "tag" {
+  description = "The GCP network tag to apply to the bastion host for firewall rules. Defaults to 'public', the expected public tag of this module."
+  type        = string
+  default     = "public"
+}
+
+variable "static_ip" {
+  description = "A static IP address to attach to the instance. The default will allocate an ephemeral IP"
+  type        = string
+  default     = null
+}
